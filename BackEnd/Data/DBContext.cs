84b08790f016020b277b8e59e4157150f741527a
Esraa Soliman <ee341110@gmail.com>
2025-10-30 18:25:03 +0300

    fix error

--- BackEnd/Data/DBContext.cs
+++ BackEnd/Data/DBContext.cs
@@ -45,7 +45,7 @@ namespace DalilKalba.Data
                     Id = 1,
                     Title = "محمية القرم",
                     Content = "واحدة من أهم المحميات الطبيعية في الدولة وتضم تنوعاً بيئياً غنياً.",
-                    IsFeatured = False,
+                    IsFeatured = false,
                     ImageUrl = "https://images.pexels.com/photos/6780348/pexels-photo-6780348.jpeg",
                     CreatedAt = new DateTime(2025, 8, 25)
                 },
@@ -54,7 +54,7 @@ namespace DalilKalba.Data
                     Id = 2,
                     Title = "شاطئ كلباء",
                     Content = "شاطئ هادئ يتميز بمياهه النقية وإطلالاته الرائعة على خليج عمان.",
-                    IsFeatured = False,
+                    IsFeatured = false,
                     ImageUrl = "https://www.propertyfinder.ae/blog/wp-content/uploads/2025/06/01-72.webp",
                     CreatedAt = new DateTime(2025, 8, 25)
                 },
@@ -63,7 +63,7 @@ namespace DalilKalba.Data
                     Id = 3,
                     Title = "بحيرة كلباء",
                     Content = "بحيرة كلباء هادئة وجميلة تحيط بها الطبيعة الخلابة",
-                    IsFeatured = False,
+                    IsFeatured = false,
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Kalba_Fort.jpg/500px-Kalba_Fort.jpg",
                     CreatedAt = new DateTime(2025, 8, 25)
                 }
