ad15ab588786be87b894ef365a47625adcb9e100
Esraa Soliman <ee341110@gmail.com>
2025-11-18 20:47:46 +0200

    fix time

--- BackEnd/obj/Debug/net9.0/DalilKalba.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net9.0/DalilKalba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DalilKalba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0708a2555c97710d665f13edeef31a1eccd3fb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5069d7c8bd2e17be0b7d918d22606b56b78c064f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DalilKalba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DalilKalba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
